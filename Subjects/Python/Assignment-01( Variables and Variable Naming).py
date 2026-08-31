dea8f04b97e03c828914df711e8a2bca3b50f195
Piyush <piyuush.cg@gmail.com>
2026-08-31 14:50:11 +0530

    Added new file again

---
+++ Subjects/Python/Assignment-01( Variables and Variable Naming).py
@@ -0,0 +1,74 @@
+#Topic 1: Variables and Variable Naming
+
+#question-1
+# name = "Raju"
+# print("Name:", name)
+
+
+#question-2
+# student_name = "Aman"
+# print("Student Name:", Student_name)
+
+
+#question-3
+# student_name = "Rahul"
+# print("Student Name:", student_name)
+
+
+#question-4
+# student_name = "Ravi"
+# print("Student Name:", student_name)
+
+
+#question-5
+# student_name = "Rohan"
+# print("Student Name:", student_name)
+
+#question-6
+# student_name = "Neha"
+# student_age = 19
+
+
+#question-7
+# print("Name:", student_name)
+# print("Age:", student_age)
+# student_name = "Karan"
+# student_name = "Raj"
+# print("Student 1:", student_name)
+# print("Student 2:", student_name)
+
+
+#question-8
+# student_name = "Priya"
+# student_age = 19
+# print("Name:", student_name)
+# print("Age:", student_age)
+
+
+#question-9
+# name = "Amit"
+# student_age = 18
+# print("Name:", name)
+# print("Age:", student_age)
+
+
+# #question-10
+# college_name = "ABC College"
+# student_name = "Meera"
+# print("College:", college_name)
+# print("Student:", student_name)
+
+
+
+# #question-11
+# first_name = "Raj"
+# last_name = "Patel"
+
+# print("Full Name:", first_name, last_name)
+
+
+# #question-12
+# first_name = "Raj"
+# last_name = "Patel"
+
+# print("Full Name:", first_name, last_name)
---
+++ Subjects/Python/Assignment-01( print()).py
@@ -0,0 +1,72 @@
+# #question-13
+# name="Rahul Raj"
+# age=18
+# course="B.Tech"
+# city="Patna"
+# print("My name is",name+". I am",age,"old. I am studying",course,"and I live in",city+".")
+
+
+# #question-14
+# name="Aman Kumar"
+# roll_number=101
+# branch="Computer Science"
+# semester=1
+# print("My name is",name+". My roll number is",str(roll_number)+". i am studying",branch,"in semester",semester)
+
+
+
+# #question-15
+# name="Priya Sharma"
+# age=19
+# gender="Female"
+# city="Jaipur"
+# print("My name is",name+". I am",age,"year old. I am",gender,"and I live in",city)
+
+
+# #question-16
+# brand="Sumsung"
+# model="Galaxy A55"
+# ram="8 GB"
+# storage="256 GB"
+# print("I have a",brand,model,"with",ram,"RAM and",storage,"storange.")
+
+
+
+#question-17
+# name="Rohit Kumar"
+# job_role="Software Developer"
+# company="ABC Technologies"
+# experience="2 year"
+# print("My name is",name+". I work as a",job_role,"at",company,'and I have',experience,"of experience.")
+
+
+# #question-18
+# college="XYZ Institute of Technology"
+# course="B.Tech"
+# branch="Information Technology"
+# city="Ahmedabad"
+# print("I am studying",course,"in",branch,"at",college,"in",city+".")
+
+
+# #question-19
+# product="Laptop"
+# brand="HP"
+# price=55000
+# quantity=1
+# print("I bought",quantity,brand,product,"for",price,"rupees.")
+
+
+# #question-20
+# name="Rahul Raj"
+# father_name="Rajesh Kumar"
+# mother_name="Sunita Kumar"
+# city="Panta"
+# print("My name is",name+". My father's name is",father_name+". My mother's name is",mother_name+". I live in",city)
+
+#question-21
+name="Neha Sharam"
+subject="Python Programming"
+marks=88
+grande="A"
+Precentage=88.0
+print(name,"scored",marks,"marks in",subject)
