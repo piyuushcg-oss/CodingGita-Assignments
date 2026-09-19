1e5c0bdd9bf3d3e2bd68b79fb7ac51bd16e65bbf
Piyush <piyuush.cg@gmail.com>
2026-09-19 14:31:00 +0530

    added some new python file

--- Subjects/Python/Assignment-03(Integrated Problems).py
+++ Subjects/Python/Assignment-03(Integrated Problems).py
@@ -1,10 +1,13 @@
-#question-60
+# # question-60
 # name=str(input("Enter students name:"))
 # marks=input("Enter 3 subject marks:").split()
 # a,b,c=marks
 # a=int(a)
 # b=int(b)
 # c=int(c)
+# # print(a)
+# # print(b)
+# # print(c)
 # total=(a + b + c)
 # avg=total//3
 
@@ -17,5 +20,74 @@
 
 
 
+
+# # #question-61
+# original_id=(input("enter your ID (as Btech-23-cse-109): ").split("-"))
+# a,b,c,d=original_id
+# a=str(a)
+# b=int(b)
+# c=str(c)
+# d=int(d)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(f"Degree:  \t{a}"f"\nBatch:  \t{b}"f"\nBranch:  \t{c}"f"\nRoll Number: \t{d}")
+
+
+
+
+
+
+
+
+
+# # #question-62
+# id=(input("Enter your full name:").split())
+# a,b,c=id
+# print(a)
+# print(b)
+# print(c)
+# print(a+"."+c)
+
+
+
+
+
+
+
+
+
+
+
+# #question-63
+# take=input('Enter a sentence:').split()
+# a,b,c,d=take
+# print(f"First Word:\t{a}"f"\nLast Word:\t{d}")
+
+
+
+
+
+
+
+
+
+
+# #question-64
+# info=input("Enter your info(rahul@gmail.com):")
+# print(f"@ Present: {'@'in info}")
+# a,b=info.split("@")
+# print(f"Username:{a}"f"\nDomain:\t {b}")
+
+
+
+
+
+
+
+
+
+#question-65
 
 
